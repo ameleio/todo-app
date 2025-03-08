f683a6e30f38830152bc872d9f2538f35dbd3b47
Amrei <amre.dasenbrook.de>
2025-03-08 23:36:42 +0100

    Änderung hinzugefügt

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Aufgabe hinzufügen, wenn Enter gedrückt wird oder Button geklickt wird
     newTaskInput.addEventListener("click", function () {
         addNewTask();
-        }
     });
+  
 
     newTaskInput.addEventListener("blur", function () {
         if (newTaskInput.value.trim() === "") {
